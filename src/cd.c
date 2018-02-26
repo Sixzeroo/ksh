3ca0c0234419677b7b547322f45606612cd1ab28
sixzeroo <sixzeroo60@gmail.com>
2018-02-26 09:19:03 +0800

    [feature] cd实现

---
+++ src/cd.c
@@ -0,0 +1,16 @@
+/**
+   @brief Bultin command: change directory.
+   @param args List of args.  args[0] is "cd".  args[1] is the directory.
+   @return Always returns 1, to continue executing.
+ */
+int lsh_cd(char **args)
+{
+    if (args[1] == NULL) {
+        fprintf(stderr, "ksh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("ksh");
+        }
+    }
+    return 1;
+}
--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "cd.c"
 
 /*
   Function Declarations for builtin shell commands:
@@ -33,23 +34,6 @@ int lsh_num_builtins() {
 /*
   Builtin function implementations.
 */
-
-/**
-   @brief Bultin command: change directory.
-   @param args List of args.  args[0] is "cd".  args[1] is the directory.
-   @return Always returns 1, to continue executing.
- */
-int lsh_cd(char **args)
-{
-  if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-  } else {
-    if (chdir(args[1]) != 0) {
-      perror("lsh");
-    }
-  }
-  return 1;
-}
 
 /**
    @brief Builtin command: print help.
@@ -131,6 +115,7 @@ int lsh_execute(char **args)
     }
   }
 
+  printf("Use system shell:\n");
   return lsh_launch(args);
 }
 
