1993f95adb565be25e4d8a810b90fe002f6f6f14
sixzeroo <sixzeroo60@gmail.com>
2018-02-27 14:56:56 +0800

    [feature] ls命令支持-al参数

--- src/ls.c
+++ src/ls.c
@@ -2,31 +2,60 @@
 #include <string.h>
 #include "utils.c"
 
-// TODO: 增加对-l参数支持
 
 int ksh_ls(char **args)
 {
     DIR *dirptr = NULL;
     struct dirent *entry;
+    int l_symbol = 0, a_symbol = 0;
 
     int len = getArgslen(args);
 
-    if(len == 1)
-        dirptr = opendir(".");
-    else if(len == 2)
-        dirptr = opendir(args[1]);
-    else
-        printf("Input error!\n");
+    dirptr = opendir(".");
+
+    // 解析参数
+    for(int i=1;i<len;i++)
+    {
+        if(args[i][0] == '-')
+        {
+            for(int j=1;args[i][j]!='\0';j++)
+            {
+                if(args[i][j] == 'l')
+                    l_symbol = 1;
+                else if (args[i][j] == 'a')
+                    a_symbol = 1;
+                else
+                {
+                    printf("Input Option is invalid!\n");
+                    return 0;
+                }
+            }
+        }
+        else
+        {
+            dirptr = opendir(args[i]);
+        }
+    }
+
     if(dirptr == NULL)
         perror("ksh");
+
+    // 读取文件信息
     while ((entry = readdir(dirptr)) != 0)
     {
-        if(strcmp(entry->d_name, ".") == 0)
-            continue;
-        if(strcmp(entry->d_name, "..") == 0)
-            continue;
-        printf("%s ", entry->d_name);
+        if(!a_symbol)
+        {
+            if(entry->d_name[0] == '.')
+                continue;
+        }
+        if(l_symbol)
+        {
+            printf("%s\t%s\n", entry->d_name, entry->d_type==4?"dir":"file");
+        }
+        else
+            printf("%s\t", entry->d_name);
     }
+
     printf("\n");
     closedir(dirptr);
     return 1;
