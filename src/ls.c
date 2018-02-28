992e58660b4a18d67a6326378d522db2dbc02268
sixzeroo <sixzeroo60@gmail.com>
2018-02-28 16:57:06 +0800

    [feature] ls命令添加展示权限 输出优化

--- src/ls.c
+++ src/ls.c
@@ -1,6 +1,34 @@
 #include <dirent.h>
 #include <string.h>
+#include <string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
 #include "utils.c"
+
+// 返回文件模式的权限信息
+int ksh_getpermission(char *fileName, char *temp)
+{
+    struct stat file_stat;
+
+    if(0 > stat(fileName, &file_stat))
+        perror("ksh");
+
+    mode_t mode = file_stat.st_mode;
+    // 文件的类型和存取的权限数据结构  值为00400
+    mode_t perm = S_IREAD;
+
+    if(S_IRUSR & mode) temp[0]='r';else temp[0]='-';
+    if(S_IWUSR & mode) temp[1]='w';else temp[1]='-';
+    if(S_IXUSR & mode) temp[2]='x';else temp[2]='-';
+    if(S_IRGRP & mode) temp[3]='r';else temp[3]='-';
+    if(S_IWGRP & mode) temp[4]='w';else temp[4]='-';
+    if(S_IXGRP & mode) temp[5]='x';else temp[5]='-';
+    if(S_IROTH & mode) temp[6]='r';else temp[6]='-';
+    if(S_IWOTH & mode) temp[7]='w';else temp[7]='-';
+    if(S_IXOTH & mode) temp[8]='x';else temp[8]='-';
+
+    return 1;
+}
 
 
 int ksh_ls(char **args)
@@ -40,23 +68,41 @@ int ksh_ls(char **args)
     if(dirptr == NULL)
         perror("ksh");
 
+    if(l_symbol)
+        printf("%-30s\t%-13s\t%-8s\t%-8s\t%-6s\n","名称", "权限", "拥有者ID", "拥有者群组ID", "类型");
+
     // 读取文件信息
     while ((entry = readdir(dirptr)) != 0)
     {
         if(!a_symbol && entry->d_name[0] == '.')
             continue;
+        // 文件权限承载字符串
+        char filemod[10] = {'-','-','-','-','-','-','-','-','-','\0'};
+
+        struct stat file_stat;
+        if(0 > stat(entry->d_name, &file_stat))
+            perror("ksh");
+
         // 文件夹显示不同的信息
         if(entry->d_type == 4)
         {
             if(l_symbol)
-                printf("\e[0;36m %s\t%s\n\e[0m",entry->d_name,"dir");
+            {
+                int res = ksh_getpermission(entry->d_name, filemod);
+                printf("\e[0;36m %-30s\t%-13s\t%-8d\t%-8d\t%-6s\n\e[0m",entry->d_name, filemod, file_stat.st_uid, file_stat.st_gid, "dir");
+
+            }
             else
                 printf("\e[0;36m %s\t\e[0m", entry->d_name);
         }
         else
         {
             if(l_symbol)
-                printf("%s\t%s\n",entry->d_name,"file");
+            {
+                int res = ksh_getpermission(entry->d_name, filemod);
+                printf("%-30s\t%-13s\t%-8d\t%-8d\t%-6s\n",entry->d_name, filemod, file_stat.st_uid, file_stat.st_gid, "file");
+
+            }
             else
                 printf("%s\t", entry->d_name);
         }
